f62be6a4df78583d609e5a4aaf1067bf8cbf3eab
derek-le1105 <derekle1105@gmail.com>
2022-05-05 23:59:24 -0700

    Set up test code for displaying sample tweet text

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from re import A
+from matplotlib.pyplot import text
 import tweepy
 import configparser
 import pandas as pd
@@ -12,37 +14,27 @@ if __name__ == "__main__":
     api_key_secret = config['twitter']['api_key_secret']
     access_token = config['twitter']['access_token']
     access_token_secret = config['twitter']['access_token_secret']
-    #bearer_token = config['twitter']['bearer_token']
 
-    # auth = tweepy.OAuthHandler(api_key, api_key_secret)
-    # auth.set_access_token(access_token, access_token)
-
-    # api = tweepy.API(auth)
-
-    # public_tweets = api.get_followers()
-    # print(public_tweets)
-    # client = tweepy.Client(
-    #     consumer_key=api_key,
-    #     consumer_secret=api_key_secret,
-    #     access_token=access_token, 
-    #     access_token_secret=access_token_secret
-    # )
     client = tweepy.Client(bearer_token=bearer_token)
-
-    response = client.search_recent_tweets(
-    "Tweepy", tweet_fields=["created_at", "lang"]
-    )
-    tweets = response.data
-
-    # You can then access those fields as attributes of the Tweet objects
-    for tweet in tweets:
-        print(tweet.id, tweet.lang)
-
-    # Alternatively, you can also access fields as keys, like a dictionary
-    for tweet in tweets:
-        print(tweet["id"], tweet["lang"])
-
-    # There’s also a data attribute/key that provides the entire data dictionary
-    for tweet in tweets:
-        print(tweet.data)
+    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+  
+    # set access to user's access key and access secret 
+    auth.set_access_token(access_token, access_token_secret)
+    
+    # calling the api 
+    api = tweepy.API(auth)
+    tweet_ids = [1460323737035677698, 1293593516040269825, 1293595870563381249]
+    id = 1272771459249844224
+    
+    # fetching the status
+    #status = api.get_status(id)
+    
+    # fetching the text attribute
+    #text = status.text 
+    
+    #print("The text of the status is : \n\n" + text)
+    for ids in tweet_ids:
+        status = api.get_status(ids)
+        text = status.text
+        print(text)
     
