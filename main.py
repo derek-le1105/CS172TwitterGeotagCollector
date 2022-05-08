42df964315b916211bd3e2c7fceae6996dc9e4a3
Derek Le <derekle1105@gmail.com>
2022-05-08 09:48:05 -0700

    testing with streamingClient and filter

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ import configparser
 import pandas as pd
 import json
 
-maxFileSize = 1e+7
+class streamListener(tweepy.StreamingClient):
+    # def on_tweet(self, tweet):
+    #     print(tweet.id)
+    #     #return super().on_tweet(tweet)
+    def on_data(self, data):
+        print(data)
+        #return super().on_includes(includes)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -23,46 +29,9 @@ if __name__ == "__main__":
   
     # set access to user's access key and access secret 
     auth.set_access_token(access_token, access_token_secret)
-    
-    # calling the api 
-    api = tweepy.API(auth)
-    tweet_ids = [1460323737035677698, 1293593516040269825, 1293595870563381249]
-    id = 1272771459249844224
-    
-    # fetching the status
-    #status = api.get_status(id)
-    
-    # fetching the text attribute
-    #text = status.text 
-    queryList = ['Orange County', 'Los Angeles', 'Riverside', 'dog', 'nba', 'met gala', 'johnny depp', 'amber heard']
-    #print("The text of the status is : \n\n" + text)
-    
-    # The method returns a Response object, a named tuple with data, includes,
-    # errors, and meta fields
-    #print(response.meta)
 
-    # In this case, the data field of the Response returned is a list of Tweet
-    # objects
+    streaming_client = streamListener(bearer_token)
+    streaming_client.add_rules(tweepy.StreamRule('has:geo'))
+    streaming_client.filter()
 
-    test = []
-    # Each Tweet object has default ID and text fields
-
-    for query in queryList:
-        #print(query)
-        response = client.search_recent_tweets(query=query, tweet_fields=['geo', 'created_at', 'author_id'], max_results=100)    
-        tweets = response.data
-        for tweet in tweets:
-            #temp = api.get_status(tweet.id)
-            if tweet.geo != None:# or temp.place != None:
-               json_object = json.dumps(tweet.data)
-               print(json_object)
-               test.append(json_object)
-        
-    with open("sample.txt", "w") as outfile:
-        for i in test:
-            outfile.write(i + '\n')
-        print(outfile.tell())
-    # By default, this endpoint/method returns 10 results
-    # You can retrieve up to 100 Tweets by specifying max_results
-    #print(test)
     
