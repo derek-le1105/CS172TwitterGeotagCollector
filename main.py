519348106ddd0d96147f9851cca41435bfcc7ec8
Derek Le <derekle1105@gmail.com>
2022-05-08 12:29:50 -0700

    Implemented file output with sample streaming

--- main.py
+++ main.py
@@ -4,19 +4,34 @@ import tweepy
 import configparser
 import pandas as pd
 import json
+import re
 
-class streamListener(tweepy.StreamingClient):
-    # def on_tweet(self, tweet):
-    #     print(tweet.id)
-    #     #return super().on_tweet(tweet)
-    def on_data(self, data):
-        print(data)
-        #return super().on_includes(includes)
-
+maxFileSize = 1e+7
 config = configparser.ConfigParser()
 config.read('config.ini')
-
 bearer_token = "AAAAAAAAAAAAAAAAAAAAAHnicAEAAAAANDnnmfkWKi6JI0bPplQs%2B35dj8o%3DxdfVtJx7kfob9G3N4aYjmLR7DWlpXGiShWqxcsY4HsckOBMZMi"
+
+class streamListener(tweepy.StreamingClient):
+    def on_tweet(self, tweet):
+        currDict = {
+            'id': tweet.id,
+            'text': tweet.text,
+            'created_at':tweet.created_at,
+            'links':self.Find(tweet.text),
+            'geo':tweet.geo
+        }
+        jsonObj = json.dumps(currDict, default=str)
+        fp = open('sample.txt','a')
+        print(jsonObj,file=fp)
+        if fp.tell() > maxFileSize:
+            self.disconnect()
+
+    def Find(self, string):    #retrieved from https://www.geeksforgeeks.org/python-check-url-string/
+        # findall() has been used 
+        # with valid conditions for urls in string
+        regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+        url = re.findall(regex,string)      
+        return [x[0] for x in url]
 
 if __name__ == "__main__":
     api_key = config['twitter']['api_key']
@@ -31,7 +46,8 @@ if __name__ == "__main__":
     auth.set_access_token(access_token, access_token_secret)
 
     streaming_client = streamListener(bearer_token)
-    streaming_client.add_rules(tweepy.StreamRule('has:geo'))
-    streaming_client.filter()
+    streaming_client.add_rules(tweepy.StreamRule('lang:en -has:media -is:retweet'))
+    streaming_client.filter(tweet_fields=['id', 'text', 'author_id', 'created_at','geo'])
+    #streaming_client.sample(tweepy.StreamRule('-has:media'), tweet_fields=['id', 'text', 'author_id', 'created_at','geo'])
 
     
