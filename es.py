5491c2972ab571d7f89c5aa69289a76c0b337386
derek-le1105 <derekle1105@gmail.com>
2022-06-04 21:59:31 -0700

    testing elasticsearch

--- es.py
+++ es.py
@@ -1,25 +1,42 @@
 from datetime import datetime
-from elasticsearch import Elasticsearch
+from http import client
+from elasticsearch import Elasticsearch, helpers
 import configparser
 import os
+import json
 
-def getData(file):
+def getData():
     data = []
-
-    for line in open(file):
-        data += 
+    for filename in os.listdir("Sample Data"):
+        currFile = os.path.join("Sample Data", filename)
+        print(currFile)
+        if os.path.isfile(currFile):
+            #print(currFile)
+            with open(currFile, 'r') as file:
+                for line in file:
+                    #data.append(json.loads(line))
+                    docSource = json.loads(line)
+                    print(docSource)
+                    yield {
+                        "_index": "tweets",
+                        "_source": docSource,
+                    }
 
 config = configparser.ConfigParser(interpolation=None)
 config.read('config.ini')
 
-es = Elasticsearch(
+client = Elasticsearch(
     cloud_id=config['elasticsearch']['cloud_id'],
     basic_auth=(config['elasticsearch']['elastic_user'], config['elasticsearch']['elastic_pw'])
 )
 
-for filename in os.listdir("Sample Data"):
-    currFile = os.path.join("Sample Data", filename)
-    if os.path.isfile(currFile):
-        #print(currFile)
-        with open(currFile, 'r') as file:
-            line = file.read()
+try:
+    # make the bulk call using 'actions' and get a response
+    resp = helpers.bulk(
+        client,
+        getData()
+    )
+    print ("\nhelpers.bulk() RESPONSE:", resp)
+    print ("RESPONSE TYPE:", type(resp))
+except Exception as err:
+    print("\nhelpers.bulk() ERROR:", err)
